0d25e880554a42de76ca5899b4dba337d1490cc3
Marcel Miklosik <marcel.miklosik@nokia.com>
2025-11-13 10:37:23 +0100

    vulnMaster changes related vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths"; // Note: cmd: `npm install -D vite-tsconfig-paths` before doing this 
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths() // we will add this tsconfig paths
+  ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
